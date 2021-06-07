439d35850d9ccc26a877e426f8a5d8f9689b049d
shai horev <shaiho31@gmail.com>
2021-06-07 15:40:37 +0300

    creating li items indise todo-list

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 const todoInput = document.getElementById("todoInput")
 const todoButton = document.getElementById("todoButton")
+let ul = document.getElementById("todosList");
 
-todoInput.addEventListener("change",inputTodoValue)
 todoButton.addEventListener("click", addTodo)
-let inputValue;
 
-function inputTodoValue (e) {
-    inputValue = e.target.value
-}
+function addTodo (event) {
+    event.preventDefault();
 
-function addTodo (e) {
-    e.preventDefault();
-    console.log(inputValue);
+    const divTodo = document.createElement("div")
+    divTodo.classList.add("div-todo")
+    ul.appendChild(divTodo)
+
+    const li = document.createElement("li")
+    li.classList.add("li-todo")
+    li.innerText = todoInput.value;
+    divTodo.appendChild(li)
+
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "Delete";
+    deleteButton.classList.add("delete-todo")
+    divTodo.appendChild(deleteButton)
+
+    const editButton = document.createElement("button")
+    editButton.innerText = "Edit";
+    editButton.classList.add("edit-todo");
+    divTodo.appendChild(editButton)
+
     todoInput.value = ""
+
 }
 
